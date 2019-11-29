78ebac475b5880d8d9f409f669cd8e2beec6daf2
ericjung1212 <56619013+ericjung1212@users.noreply.github.com>
2019-11-28 19:25:44 -0500

    Update pressure-code.ino

--- Arduino Code/pressure-code/pressure-code.ino
+++ Arduino Code/pressure-code/pressure-code.ino
@@ -1,4 +1,5 @@
 #define SENSOR_PIN A0; //analog pin, takes the analog input of the FSR of 0 - 1023
+#define OUT_PIN A1; //the one pin that does not connect to the multiplexer
 #define muxPin1 10; // these three pins define the multiplexer inputs and the cycles.
 #define muxPin2 11;
 #define muxPin3 12;
@@ -6,27 +7,31 @@
 void setup() {
   Serial.begin(9600); //baud rate 9600
   pinMode(SENSOR_PIN, INPUT); //analog pin is the only input
+  pinMode(OUT_PIN, INPUT);
   pinMode(muxPin1, OUTPUT); //the mux pins are OUTPUT as they cycle through the multiplexer
   pinMode(muxPin2, OUTPUT);
   pinMode(muxPin3, OUTPUT);
 }
 
 void loop() {
-  for (i = 0, i < 9, i++) //for loops 9 times, for 9 values.
+  for (i = 0, i < 8, i++) //for loops 8 times, for 8 values.
   {
     int value = cycle_mux(i); //calls to mux cycling method and receives a value.
     
     send_string(value); //sends above value to the method that sends the values to the serial port
   }
-  delay(1000); //NEED TO ASK ANDREA: delay is put here so that each string send per seconds is all 9 values at once. Or should it be 1 of the 9 values sent per second, so a 9 second cycle?
+  int outreading = analogRead(OUT_PIN);
+  send_string(OUT_PIN);
+    
+  delay(500);
 }
 
-int cycle_mux(int channel) { //method to cycle through the multiplexer 9 times. 
-  digitalWrite(muxPin0, bitRead(channel, 0)); // these three digital writes cycle through all possible 3-bit binary values, which there are NINE of. This cycles the mux 9 times for 9 different inputs.
-  digitalWrite(muxPin1, bitRead(channel, 1)); //I guess cycling through the values and giving them to the mux changes the "channel"?
+int cycle_mux(int channel) { //method to cycle through the multiplexer 8 times. 
+  digitalWrite(muxPin0, bitRead(channel, 0)); 
+  digitalWrite(muxPin1, bitRead(channel, 1)); 
   digitalWrite(muxPin2, bitRead(channel, 2));
 
-  int muxValue = analogRead(SENSOR_PIN); //the mux value is sent, before it cycles back to get a new 3-bit value, changing the channel therefore a new value.
+  int muxValue = analogRead(SENSOR_PIN);
 
   return muxValue;
 }
