8c2717c707acfe92068ddae3e275a7050003d4c3
ericjung1212 <56619013+ericjung1212@users.noreply.github.com>
2020-01-26 21:27:49 -0500

    force calculations
    Added force calculations to the Arduino code so it sends Newtons to the GUI. Need to add area values to start sending pascals?

--- Arduino Code/pressure-code/pressure-code.ino
+++ Arduino Code/pressure-code/pressure-code.ino
@@ -3,26 +3,62 @@
 #define muxPin1 10; //these three pins define the multiplexer inputs and the cycles.
 #define muxPin2 11;
 #define muxPin3 12;
+#define LEDpin 13; //the pin to control threshold LED
+double valueVoltage;
+double valueResistance;
+double valueResistor = 1000000; // insert value of resistors
+double valueConductance;
+double valueForce;
 
 void setup() {
   Serial.begin(9600); //baud rate 9600
-  pinMode(SENSOR_PIN, INPUT); //analog pin is the only input
+  pinMode(SENSOR_PIN, INPUT); //analog pin is the input
   pinMode(OUT_PIN, INPUT);
   pinMode(muxPin1, OUTPUT); //the mux pins are OUTPUT as they cycle through the multiplexer
   pinMode(muxPin2, OUTPUT);
   pinMode(muxPin3, OUTPUT);
+  pinMode(LEDpin, INPUT);
+  digitalWrite(LEDpin, LOW); //sets threshole LED to off at start
 }
 
 void loop() {
   for (i = 0, i < 8, i++) //for loops 8 times, for 8 values.
   {
     int value = cycle_mux(i); //calls to mux cycling method and receives a value.
-    
-    send_string(value); //sends above value to the method that sends the values to the serial port
+
+    value = map(valueVoltage, 0, 1023, 5000);
+    valueResistance = 5000 - valueVoltage;
+    valueResistance *= 10 000;
+    valueResistance /= valueVoltage;
+    valueConductance = valueResistor / valueResistance;
+
+    if (valueConductance <= 1000) {
+      valueForce = valueConductance / 80;
+      send_string(valueForce); //sends above value to the method that sends the values to the serial port
+    }else {
+      valueForce = valueConductance - 1000;
+      valueForce /= 30;
+      send_string(valueForce)
+    }   
   }
   int outreading = analogRead(OUT_PIN);
   send_string(OUT_PIN);
-    
+
+  if (Serial.available())
+  {
+     char val = Serial.read();
+
+     if (val = 'a') //When threshold is exceeded, the GUI sends the char 'a' triggering the LED to light up
+     {
+        digitalWrite(LEDpin, HIGH);
+     }
+
+     if (val = 'b') //When user needs to reset system and turn off light, GUI sends the char 'b' to turn off LED
+     {
+      digitalWrite(LEDpin, LOW);
+     }     
+  }
+  
   delay(500);
 }
 
---
+++ LED tester/LED-TEST/LED-TEST.ino
@@ -0,0 +1,14 @@
+#define LED_PIN 1
+#define LED_INPUT 2
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_INPUT, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  
+}
+
+void loop() {
+  
+
+}
